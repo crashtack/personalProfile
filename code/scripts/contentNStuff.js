453c302fa5c4c283f6697be53c7c0e22f075fd8e
crashtack <crashtack@gmail.com>
2016-06-09 23:51:22 -0700

    added script/contentNStuff.js

---
+++ code/scripts/contentNStuff.js
@@ -0,0 +1,33 @@
+var dataNStuff = [
+  {
+    title: 'Front',
+    catagory: 'external',
+    revision: '1.0',
+    athor: 'blabla',
+    description: '<p>The Front View, Blaaaa Bladiiii Blaaaa, to the blaaa...est'
+  },
+  {
+    title: 'Rear',
+    catagory: 'external',
+    revision: '1.0',
+    athor: 'blabla',
+    description: '<p>The rear view. Blaaaa Bladiiii Blaaaa, to the blaaa...est'
+  },
+  {
+    title: 'First Floor',
+    catagory: 'internal',
+    revision: '1.0',
+    athor: 'blabla',
+    description: '<p>Fist Floor internal, laaaa Bladiiii Blaaaa, to the blaaa...est'
+  },
+  {
+    title: 'Upstairs',
+    catagory: 'external',
+    revision: '1.0',
+    athor: 'blabla',
+    description: '<p>Upstairs, Blaaaa Bladiiii Blaaaa, to the blaaa...est'
+  },
+
+
+
+];
