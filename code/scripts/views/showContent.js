6bd665ce41fb00d662e85327d5ae033c81b70890
crashtack <crashtack@gmail.com>
2016-06-20 22:53:56 -0700

    adding some functionality to showContent.js

--- code/scripts/views/showContent.js
+++ code/scripts/views/showContent.js
@@ -1,35 +1,90 @@
-var content = [];
+(function(module) {
 
-function Content (opts) {
-  this.title = opts.title;
-  this.page = opts.page;
-  this.category = opts.category;
-  this.revision = opts.revision;
-  this.author = opts.author;
-  this.description = opts.description;
-}
+  function Article (opts) {
+    Object.keys(opts).forEach(function(e, index, keys) {
+      this[e] = opts[e];
+    },this);
+  }
 
-Content.prototype.toHtml = function (sourceTemplate) {
-var template = Handlebars.compile($(sourceTemplate).text());
-var source = $('#content-template').html();
-//var template = Handlebars.compile(source);
+  Content.prototype.toHtml = function (sourceTemplate) {
+    var template = Handlebars.compile($(scriptTemplateId).text());
 
+    this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+    this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
+    this.body = marked(this.body);
 
+    return template(this);
+  };
 
+  Content.createTable = function() {
+    webDB.execute(
+      'CREATE TABLE IF NOT EXISTS articles (' +
+        'id INTEGER PRIMARY KEY, ' +
+        'title VARCHAR(255) NOT NULL, ' +
+        'page VARCHAR(255) NOT NULL, ' +
+        'category VARCHAR(20), ' +
+        'revision VARCHAR(20), ' +
+        'publishedOn DATETIME, ' +
+        'author VARCHAR(30), ' +
+        'body TEXT NOT NULL);',
+      function() {
+        console.log('Successfully set up the articles table.');
+      }
+    );
+  };
 
+  Content.prototype.insertRecord = function() {
+    webDB.execute(
+      [
+        {
+          'sql': 'INSERT INTO articles (title, page, category, revision, publishedOn, author, body) VALUES (?, ?, ?, ?, ?, ?, ?);',
+          'data': [this.title, this.page, this.category, this.revision, this.publishedOn, this.author, this.body],
+        }
+      ]
+    );
+  };
 
-var appTemplate = $('#content-template').html();
-var compiledTemplate = Handlebars.compile(appTemplate);
-var html = compiledTemplate(data);
-$('#showContentApp').append(html);
-};
+  Content.loadAll = function(rows) {
+    Article.allArticles = rows.map(function(ele) {
+      return new Article(ele);
+    });
+  };
 
-content.forEach(function(a) {
-if ($('#category-filter:contains("' + a.category + '")').length === 0 ) {
-  $('#category-filter').append(a.toHtml('#category-filter-template'));
-}
+  Content.fetchAll = function() {
+    webDB.execute('SELECT * FROM articles ORDER BY publishedOn DESC', function(rows) {
+      if (rows.length) {
+        Article.loadAll(rows);
+        articleView.renderIndexPage();
+        articleView.initAdminPage();
+      } else {
+        $.getJSON('/data/hackerIpsum.json', function(rawData) {
+          // Cache the json, so we don't need to request it next time:
+          rawData.forEach(function(item) {
+            var article = new Article(item); // Instantiate an article based on item from JSON
+            article.insertRecord(); // Cache the article in DB
+          });
+          webDB.execute('SELECT * FROM articles ORDER BY publishedOn DESC', function(rows) {
+            Article.loadAll(rows);
+            articleView.renderIndexPage();
+            articleView.initAdminPage();
+          });
+        });
+      }
+    });
+  };
 
-//$('#aurthor-filter').append(a.toHtml('#aurthor-filter-template'));
-$('#articles').append(a.toHtml('#article-template'));
+  content.forEach(function(a) {
+  if ($('#category-filter:contains("' + a.category + '")').length === 0 ) {
+    $('#category-filter').append(a.toHtml('#category-filter-template'));
+  }
 
-});
+  //$('#aurthor-filter').append(a.toHtml('#aurthor-filter-template'));
+  $('#articles').append(a.toHtml('#article-template'));
+
+  });
+
+  Content.createTable();
+  Content.fetchAll();
+
+  module.Content = Content;
+})(window);
