a3525edec9e49807cae8bfe2982acff2ca7090a8
crashtack <crashtack@gmail.com>
2016-06-22 15:11:49 -0700

    fixed error in server.js

--- code/server.js
+++ code/server.js
@@ -15,7 +15,7 @@ app.get('/github/*', proxyGitHub);
 
 app.use(express.static('./'));
 
-app.get('*', function(request, respose) {
+app.get('*', function(request, response) {
   console.log('New request:', request.url);
   response.sendFile('index.html', {root: '.'});
 });
