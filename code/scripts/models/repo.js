1d7f426177d275d8d8933eaf48cb664b89b4581f
crashtack <crashtack@gmail.com>
2016-06-22 14:44:10 -0700

    refatored repos.requestRepos

--- code/scripts/models/repo.js
+++ code/scripts/models/repo.js
@@ -4,21 +4,28 @@
   repos.allRepos = [];
 
   repos.requestRepos = function(callback) {
-    /* TODO: How would you like to fetch your repos? Someone say AJAX?!
-      Do not forget to call the callback! */
-    $.ajax({
-      url: 'https://api.github.com/users/crashtack/repos' +
-        '?per_page=10' +
-        '&sort=updated',
-      typeof: 'GET',
-      headers: {'Authorization': 'token ' + githubToken},
-      success: function(data) {
-        console.log(data);
-        repos.allRepos = data;
-        callback();
-      }
+    // TODO: Refactor this ajax method into a get method to the proxy
+    //  'end point' provided by server.js.
+    repos.requestRepos = function(callback) {
+      $.get('/github/users/crashtack/repos' +
+              '?per_page=10&sort=updated').done(function(data) {
+                repos.allRepos = data;
+              }).done(callback);
 
-    });
+
+    // $.ajax({
+    //   url: 'https://api.github.com/users/crashtack/repos' +
+    //     '?per_page=10' +
+    //     '&sort=updated',
+    //   typeof: 'GET',
+    //   headers: {'Authorization': 'token ' + githubToken},
+    //   success: function(data) {
+    //     console.log(data);
+    //     repos.allRepos = data;
+    //     callback();
+    //   }
+    //
+    // });
 
   };
 
