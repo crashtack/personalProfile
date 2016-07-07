7f0d4a5d083dd8070f7432d6aac9867e90f7c1b7
crashtack <crashtack@gmail.com>
2016-07-07 14:03:04 -0700

    refactoring html and js to use Jquery templete added content template to index.html added Content toHtml function to fill template with data from contentNStuff.js

--- scripts/models/content2.js
+++ scripts/models/content2.js
@@ -11,5 +11,40 @@ function Content (opts) {
 };
 
 Content.protoype.toHtml = function() {
-  var $new
+  var $newArticle = $('article.template').clone();
+  $newArticle.attr('data-catagory', this.category);
+
+  $newArticle.find('h1').html(this.titile);
+  $newArticle.find('section.page').html(this.page);
+  $newArticle.find('section.category').html(this.category);
+  $newArticle.find('section.revision').html(this.revision);
+  $newArticle.find('section.author').html(this.author);
+  $newArticle.find('section.article-body').html(this.body);
+
+  // Display the date as a relative number of "days ago":
+  $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
+  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
+  /* TODO: This cloned article is no longer a template,
+   as it now has real data attached to it! We need to account
+   for that and change it before this current article gets
+   rendered to our DOM. */
+  $newArticle.removeClass('template');
+  return $newArticle;
 };
+
+
+// Sort our data by date published, descending order
+ourLocalData.sort(function(a,b) {
+  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+});
+/* Now iterate through our transformed collection and instantiate
+ a new Article instance for each object in our collection. */
+ourLocalData.forEach(function(ele){
+  articles.push(new Article(ele));
+});
+
+/* Append each Article to the DOM.
+NOTE: Remember that the '.toHtml' method invoked is one WE created. */
+articles.forEach(function(article){
+  $('#content').append(article.toHtml());
+});
