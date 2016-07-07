e33ce7d94387eab2a74f941b3ffb2f563b5a9d95
crashtack <crashtack@gmail.com>
2016-07-07 12:58:57 -0700

    refactored contentNStuff.json to an object in contentNStuff.js

---
+++ data/contentNStuff.js
@@ -0,0 +1,39 @@
+
+var ourLocalData = [
+  {
+    title: 'Front',
+    page: 'A02',
+    category: 'external',
+    revision: '1.0',
+    publishedOn: '2016-5-28',
+    author: 'blabla',
+    body: '<p>The Front View, Blaaaa Bladiiii Blaaaa, to the blaaa...est</p>'
+  },
+  {
+    title: 'Rear',
+    page: 'A03',
+    category: 'external',
+    revision: '1.0',
+    publishedOn: '2016-5-28',
+    author: 'blabla',
+    body: '<p>The rear view. Blaaaa Bladiiii Blaaaa, to the blaaa...est</p>'
+  },
+  {
+    title: 'First Floor',
+    page: 'A04',
+    category: 'internal',
+    revision: '1.0',
+    publishedOn: '2016-5-28',
+    author: 'blabla',
+    body: '<p>Fist Floor internal, laaaa Bladiiii Blaaaa, to the blaaa...est</p>'
+  },
+  {
+    title: 'Upstairs',
+    page: 'A05',
+    category: 'external',
+    revision: '1.0',
+    publishedOn: '2016-5-28',
+    author: 'blabla',
+    body: '<p>Upstairs, Blaaaa Bladiiii Blaaaa, to the blaaa...est</p>'
+  },
+];
