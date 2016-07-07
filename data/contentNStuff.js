36d8b5b8c8df587748d5af5f92e08291072344c4
crashtack <crashtack@gmail.com>
2016-07-07 13:06:47 -0700

    use ipsum generator to add words to body

--- data/contentNStuff.js
+++ data/contentNStuff.js
@@ -14,7 +14,7 @@ var ourLocalData = [
     page: 'A03',
     category: 'external',
     revision: '1.0',
-    publishedOn: '2016-5-28',
+    publishedOn: '2016-5-5',
     author: 'blabla',
     body: '<p>The rear view. Blaaaa Bladiiii Blaaaa, to the blaaa...est</p>'
   },
@@ -23,7 +23,7 @@ var ourLocalData = [
     page: 'A04',
     category: 'internal',
     revision: '1.0',
-    publishedOn: '2016-5-28',
+    publishedOn: '2016-5-26',
     author: 'blabla',
     body: '<p>Fist Floor internal, laaaa Bladiiii Blaaaa, to the blaaa...est</p>'
   },
@@ -32,7 +32,7 @@ var ourLocalData = [
     page: 'A05',
     category: 'external',
     revision: '1.0',
-    publishedOn: '2016-5-28',
+    publishedOn: '2016-5-23',
     author: 'blabla',
     body: '<p>Upstairs, Blaaaa Bladiiii Blaaaa, to the blaaa...est</p>'
   },
