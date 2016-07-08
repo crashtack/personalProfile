60e8cbe09d21d2789d82436bf24c05519fcacfaa
crashtack <crashtack@gmail.com>
2016-07-08 13:48:56 -0700

    added content2View.js, added .populateFilters function

---
+++ scripts/models/content2View.js
@@ -0,0 +1,27 @@
+// Configure a view object, to hold functions ofr synamic updates and content related event handlebars
+
+var content2View = {};
+
+content2View.populateFilters = function() {
+  $('article').each(function() {
+    if (!$(this).hasClass('template')) {
+      var val = $(this).find('address a').text();
+      var optionTag = '<option value="' + val + '">' + val + '</option>';
+      $('category-filter').append(optionTag);
+
+      val = $(this).attr('data-category');
+      optionTag = '<option value="' + val + '">' + val + '</option>';
+      if ($('#category-filter option[value="' + val + '"]').length === 0) {
+        $('#category-filter').append(optionTag);
+      }
+    }
+  });
+};
+
+$(document).ready(function() {
+  content2View.populateFilters();
+  //articleView.handleAuthorFilter();
+  //articleView.handleCategoryFilter();
+  //articleView.handleMainNav();
+  //articleView.setTeasers();
+});
